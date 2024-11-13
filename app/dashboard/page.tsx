e7d42857e2db67ee0b285be9cbdea5a2fcc60455
Taylor Pasquariello <taylor@cascadiansoftware.com>
2024-11-13 11:29:55 -0800

    seed

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,28 @@
+'use client'
+
+import useSWR from 'swr'
+import axios from 'axios';
+
+// const fetcher = (...args) => fetch(...args).then((res) => res.json())
+const fetcher = (url: string, token?: string) => axios.get(url, {
+    headers: {
+      'Authorization': `Bearer ${token}` // Assuming it's a Bearer token
+    }
+  })
+    .then(response => {
+      // Handle the response data
+      console.log(response.data);
+    })
+    .catch(error => {
+      // Handle errors
+      console.error('Error:', error);
+    });
+
+
 export default function Page() {
+
+    // const { data: user } = useSWR(['https://app.circle.so/api/v1/headless/auth_token', process.env.CIRCLE_TOKEN], ([url, token]) => fetcher(url, token))
+    // console.log('user', user);
+
     return <p>Dashboard Page</p>;
   }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,38 @@
+'use client'
+
 import '@/app/ui/global.css';
+import '@circleco/headless-ui-kit/style.css'
 import { inter } from '@/app/ui/fonts';
+
+import {
+  CircleProvider
+} from "@circleco/headless-ui-kit/components/Provider";
+
+import {
+  BlockEditor
+} from "@circleco/headless-ui-kit/components/BlockEditor";
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+
+  const getTokenFunction = () => {
+    return '';
+    // return "<MEMEBER_TOKEN>" // you must return the member api here.
+  };
+
   return (
-    <html lang="en">
-      <body className={`${inter.className} antialiased`}>{children}</body>
-    </html>
+    
+      <html lang="en">
+        <body className={`${inter.className} antialiased`}>
+          {/* <CircleProvider getTokenFunction={getTokenFunction}>
+            <BlockEditor readOnly={true} value={{}} sgidToObjectMap={{}} inlineAttachments={[]}/> */}
+            {children}
+          {/* </CircleProvider> */}
+
+          </body>
+      </html>
   );
 }
