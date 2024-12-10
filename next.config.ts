864f39cae7c2ed893d060d68fef48b9aa90d95a8
Taylor Pasquariello <taylor@cascadiansoftware.com>
2024-12-09 20:31:40 -0800

    ignore

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,13 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   gnoreDuringBuilds: false,
-  ignoreBuildErrors: true
+  ignoreBuildErrors: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
