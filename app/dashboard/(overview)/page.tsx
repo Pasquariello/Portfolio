a070b3a6ad79af0826317a8a4ceb95b481cad494
Taylor Pasquariello <taylor@cascadiansoftware.com>
2025-01-30 19:03:21 -0800

    clean comments

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,25 +1,13 @@
 import CardWrapper from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import { lusitana } from '@/app/ui/fonts';
-import { fetchLessonData } from '@/app/lib/data';
 import { Suspense } from 'react';
 import { RevenueChartSkeleton, LatestEventsSkeleton, CardsSkeleton } from '@/app/ui/skeletons';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
-import { ProgressBar } from '@/app/components/progressBar';
 import CourseWrapper from '@/app/ui/dashboard/courses';
 
 export default async function Page() {
-    // const data =  await fetchCourseSections();
-    // const data2 = await fetchLessonData();
-    // console.log('data ===', data);
-    // console.log('data === lessons', data[0].lessons);
-    // console.log('data === 2', data2);
-
-    // const data3 = await fetchCourses();
-    // console.log('data === 3', data3);
-
-
-
+   
   return (
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
