6186ab1b1aa28c24fd44a69eeb708c410612b87e
Taylor Pasquariello <taylor@cascadiansoftware.com>
2025-02-21 16:55:31 -0800

    update mobile layout

--- app/page.tsx
+++ app/page.tsx
@@ -5,13 +5,13 @@ import VerticalNav from './components/vertical_nav';
 export default async function Page() {
 
   return (
-    <main className="flex min-h-screen flex-col ">
+    <main className="flex min-h-screen flex-col bg-black ">
       <div className='flex align-center h-screen items-center'>
         <div className='lg:pl-24 w-full'>
             <div className='flex flex-col lg:items-center text-center lg:inline-block' >
-              <h2 className={`${roboto.className} text-white text-xl sm:text-4xl font-light relative`}>Welcome to my profile!</h2>
-              <h1 className={`${titillium.className} text-white text-5xl sm:text-8xl mt-4 mb-8`}>Taylor Pasquariello</h1>
-              <h2 className={`${roboto.className} text-white text-xl sm:text-4xl font-light relative`}><TextLoop/></h2>
+              <h2 className={`${roboto.className} text-white text-3xl sm:text-4xl font-light relative`}>Welcome to my profile!</h2>
+              <h1 className={`${titillium.className} text-white text-6xl sm:text-8xl mt-4 mb-8`}>Taylor Pasquariello</h1>
+              <h2 className={`${roboto.className} text-white text-3xl sm:text-4xl font-light relative`}><TextLoop/></h2>
             </div>
         </div>
 
