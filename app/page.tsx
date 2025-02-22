cf80317fce729cde1b5ddf3b68522cebf0e387ef
Taylor Pasquariello <taylor@cascadiansoftware.com>
2025-02-22 00:23:29 -0800

    background update

--- app/page.tsx
+++ app/page.tsx
@@ -24,6 +24,7 @@ export default async function Page() {
         style={{
           objectFit: 'cover',
           display: 'block',
+          background: 'black',
           // width: '100%',
           // height: '100%',
           position: 'absolute',
