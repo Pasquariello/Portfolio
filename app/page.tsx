6463ac8e48244c0986a76f0824ab69c8fde1b67b
Taylor Pasquariello <taylor@cascadiansoftware.com>
2025-02-24 16:04:10 -0800

    fix text size

--- app/page.tsx
+++ app/page.tsx
@@ -110,7 +110,7 @@ export default async function Page({params}) {
         <div className='lg:pl-24 w-full'>
             <div className='lg:inline-block text-center' >
               <h2 className={`${roboto.className} text-white text-3xl sm:text-4xl font-light text-center lg:text-left`}>Welcome to my portfolio</h2>
-              <h1 className={`${titillium.className} text-white text-5xl sm:text-8xl mt-4 mb-8 tracking-widest`}>TAYLOR PASQUARIELLO</h1>
+              <h1 className={`${titillium.className} text-white text-5xl md:text-6xl lg:text-7xl 2xl:text-8xl mt-4 mb-8 tracking-widest`}>TAYLOR PASQUARIELLO</h1>
               <h2 className={`${roboto.className} text-white text-3xl sm:text-4xl font-light relative`}><TextLoop/></h2>
             </div>
         </div>
